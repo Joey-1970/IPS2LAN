cbe8849b44fd719ef0900d76afc0399827a9b1f7
Joey-1970 <paeper@horburg.de>
2020-01-22 17:55:47 +0100

    Update module.php

--- IPS2LANConfigurator/module.php
+++ IPS2LANConfigurator/module.php
@@ -201,8 +201,8 @@
 
 		$devices = array();
 
-		$Search = array(" auf ", "[ether]", "eth0", "(", ")", " at ", " on ", " en0 ", "ifscope", "[ethernet]");
-		$Replace = array(" ", " ", " ", " ", " ", " ", " ", " ", " ", " ");
+		$Search = array(" auf ", "[ether]", "eth0", "(", ")", " at ", " on ", " en0 ", "ifscope", "[ethernet]", "ovs_bond0");
+		$Replace = array(" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ");
 		
 		$Devices = array();
 
