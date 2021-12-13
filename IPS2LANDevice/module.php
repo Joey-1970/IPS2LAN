6887ae050c0a5d70556145f7bd1aa8bf0cac8ef6
Joey-1970 <paeper@horburg.de>
2021-12-13 15:42:22 +0100

    Update module.php

--- IPS2LANDevice/module.php
+++ IPS2LANDevice/module.php
@@ -172,7 +172,7 @@ class IPS2LANDevice extends IPSModule
 	public function MessageSink($TimeStamp, $SenderID, $Message, $Data)
     	{
 		switch ($Message) {
-			case 10001:
+			case IPS_KERNELSTARTED:
 				// IPS_KERNELSTARTED
 				$this->ApplyChanges();
 				break;
